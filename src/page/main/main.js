b4bfa5e85975ef15677c360e3d13188657afb6f8
Waldo J <w.saccaco@gmail.com>
2018-08-04 15:25:55 -0500

    router

---
+++ src/page/main/main.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import './main.css';
+
+export default class Main extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+
+  }
+
+  render() {
+    return (
+        <div className="Main">
+          page main
+        </div>
+    );
+  }
+
+}
