917fca9127c0fc4350a26762ef1b4559ad6db07a
jdipere <jdipere@hotmail.com>
2018-08-19 23:45:41 -0500

    app

--- src/App.js
+++ src/App.js
@@ -12,13 +12,10 @@ class App extends Component {
     return (
         <Router>
           <Switch>
-<<<<<<< HEAD
-            <Route path="/change1" component={Main}/>
 
-=======
             <Route path="/change" component={Change}/>
             <Route path="/loader-technical" component={LeaderTechnical}/>
->>>>>>> baf70071811d0f88276679b157c19a6fad909145
+
           </Switch>
         </Router>
     );
