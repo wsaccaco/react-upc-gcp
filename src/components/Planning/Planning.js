37c4cc9a93401adb997d058aa717d3204aae1ea2
ivanvelarde <u201521409@upc.edu.pe>
2018-08-30 15:35:15 -0500

    Maqueta planificacion

--- src/components/Planning/Planning.js
+++ src/components/Planning/Planning.js
@@ -3,7 +3,7 @@ import {Button, DatePicker, Form, Input, message, Popconfirm, Radio, Select, Tab
 import moment from 'moment';
 
 import './Planning.css';
-import '../FlowRequirement/FlorRequirement.css'
+import '../NewRequirementList/NewRequirementList.css'
 
 import http from '../../service/http';
 
@@ -16,39 +16,119 @@ const confirm = Modal.confirm;
 const formItemLayout = {
     labelCol: {
         xs: {span: 24},
-        sm: {span: 8},
+        sm: {span: 12},
     },
     wrapperCol: {
         xs: {span: 24},
-        sm: {span: 16},
-    },
+        sm: {span: 12},
+    }
 };
-const textEnviarRiesgo = 'Estas seguro de enviar la información a Evaluar en Riesgo?';
 const dateFormat = 'YYYY-MM-DD';
-//Cambios de Requerimientos
-const _column_template = [{
-    title: 'Código',
+
+const _column_template_requirement = [{
+    title: 'Requerimiento',
     dataIndex: 'lir_Codigo',
     key: 'lir_Codigo',
 }, {
-    title: 'Requerimiento',
+    title: 'Clasificación',
     dataIndex: 'lir_Nombre',
     key: 'lir_Nombre',
 }, {
-    title: 'Prioridad',
+    title: 'Esfuerzo',
     dataIndex: 'lir_Prioridad',
     key: 'lir_Prioridad',
 }, {
-    title: 'Estado',
+    title: 'Inicio',
     dataIndex: 'est_Estado',
     key: 'est_Estado',
 }, {
-    title: 'Fecha',
+    title: 'Termino',
+    dataIndex: 'lir_Fecha',
+    key: 'lir_Fecha',
+}, {
+    title: 'Responsable',
+    dataIndex: 'lir_Fecha',
+    key: 'lir_Fecha',
+}, {
+    title: 'Acción',
     dataIndex: 'lir_Fecha',
     key: 'lir_Fecha',
 }];
-let columns_requirement_change = [..._column_template];
-
+let columns_requirement = [..._column_template_requirement];
+const _column_template_rrhh = [{
+    title: 'Identificador',
+    dataIndex: 'lir_Codigo',
+    key: 'lir_Codigo',
+}, {
+    title: 'Perfil',
+    dataIndex: 'lir_Nombre',
+    key: 'lir_Nombre',
+}, {
+    title: 'Cantidad',
+    dataIndex: 'lir_Prioridad',
+    key: 'lir_Prioridad',
+}, {
+    title: 'Nivel',
+    dataIndex: 'est_Estado',
+    key: 'est_Estado',
+}, {
+    title: 'Desde',
+    dataIndex: 'lir_Fecha',
+    key: 'lir_Fecha',
+}, {
+    title: 'Hasta',
+    dataIndex: 'lir_Fecha',
+    key: 'lir_Fecha',
+}, {
+    title: 'Presupuesto',
+    dataIndex: 'lir_Fecha',
+    key: 'lir_Fecha',
+}, {
+    title: 'Acción',
+    dataIndex: 'lir_Fecha',
+    key: 'lir_Fecha',
+}];
+let columns_rrhh = [..._column_template_rrhh];
+const _column_template_add = [{
+    title: 'Recurso',
+    dataIndex: 'lir_Codigo',
+    key: 'lir_Codigo',
+}, {
+    title: 'Cantidad',
+    dataIndex: 'lir_Nombre',
+    key: 'lir_Nombre',
+}, {
+    title: 'Desde',
+    dataIndex: 'lir_Prioridad',
+    key: 'lir_Prioridad',
+}, {
+    title: 'Hasta',
+    dataIndex: 'est_Estado',
+    key: 'est_Estado',
+}, {
+    title: 'Presupuesto',
+    dataIndex: 'lir_Fecha',
+    key: 'lir_Fecha',
+}, {
+    title: 'Acción',
+    dataIndex: 'lir_Fecha',
+    key: 'lir_Fecha',
+}];
+let columns_requirement_add = [..._column_template_add];
+const _column_agreement = [{
+    title: 'Fecha',
+    dataIndex: 'lir_Codigo',
+    key: 'lir_Codigo',
+}, {
+    title: 'Resumen',
+    dataIndex: 'lir_Nombre',
+    key: 'lir_Nombre',
+}, {
+    title: 'Acción',
+    dataIndex: 'lir_Fecha',
+    key: 'lir_Fecha',
+}];
+let columns_agreement = [..._column_agreement];
 class Planning extends Component {
 
     constructor(props) {
@@ -336,186 +416,168 @@ class Planning extends Component {
             <Form onSubmit={this.handleSubmit} className="gcp-form" >
                 <div>
                     <Row>
+                        <Col>
+                            {getFieldDecorator('rfc_Codigo', {
+                                rules: [{required: true, message: 'Definir el RFC'}],
+                                initialValue: rfc_id
+                            })(
+                                <Input type="hidden"/>
+                            )}
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={6}>
+                            <FormItem label={'Codigo de informe:'} {...formItemLayout}>
+                                <Input disabled={true} />
+                            </FormItem>
+                        </Col>
+                        <Col span={6}>
+                            <FormItem label={'Fecha de reporte:'} {...formItemLayout} >
+                                <DatePicker
+                                    disabled={true}
+                                    value={moment(evr_FechaEnvio, this.dateformat)}
+                                    format={dateFormat} />
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={24}>
+                            <FormItem label={'Responsable:'}
+                                      labelCol={{xs: {span: 24},sm: {span: 3}}}
+                                      wrapperCol={{xs: {span: 24},sm: {span: 21}}}>
+                                <Input disabled={true} />
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={24}>
+                            <FormItem>
+                                <div className="new-requirement-list component">
+                                    <div className="wrap-table">
+                                        <Table
+                                            title={() => "Lista de requerimientos"}
+                                            bordered
+                                            locale={{emptyText: 'No hay datos'}}
+                                            size="small"
+                                            scroll={{x: 800}}
+                                            loading={loadingChange}
+                                            dataSource={dataSourceChange}
+                                            columns={columns_requirement} />
+                                    </div>
+                                </div>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={24}>
+                            <FormItem>
+                                <div className="new-requirement-list component">
+                                    <div className="wrap-table">
+                                        <Table
+                                            title={() => "RRHH Necesarios"}
+                                            bordered
+                                            locale={{emptyText: 'No hay datos'}}
+                                            size="small"
+                                            scroll={{x: 800}}
+                                            loading={loadingChange}
+                                            dataSource={dataSourceChange}
+                                            columns={columns_rrhh} />
+                                    </div>
+                                </div>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={24}>
+                            <FormItem>
+                                <div className="new-requirement-list component">
+                                    <div className="wrap-table">
+                                        <Table
+                                            title={() => "Recursos adicionales necesarios"}
+                                            bordered
+                                            locale={{emptyText: 'No hay datos'}}
+                                            size="small"
+                                            scroll={{x: 800}}
+                                            loading={loadingChange}
+                                            dataSource={dataSourceChange}
+                                            columns={columns_requirement_add} />
+                                    </div>
+                                </div>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
                         <Col span={12}>
                             <FormItem>
-                                {getFieldDecorator('rfc_Codigo', {
-                                    rules: [{required: true, message: 'Definir el RFC'}],
-                                    initialValue: rfc_id
-                                })(
-                                    <Input type="hidden"/>
-                                )}
-                            </FormItem>
-
-                            <FormItem
-                                label={'Lista de requerimientos'}
-                                {...formItemLayout}
-                            >
-                                {getFieldDecorator('evr_Requiere', {
-                                    rules: [{required: false, message: 'Definir Requiere'}],
-                                    initialValue: evr_Requiere
-                                })(
-                                    <RadioGroup
-                                        onChange={this.onChange}
-                                        disabled={evaluacionriesgoDisabled}>
-                                        <Radio value={0}>NO</Radio>
-                                        <Radio value={1}>SI</Radio>
-                                    </RadioGroup>
-                                )}
-                                <Button
-                                    type="primary"
-                                    htmlType="submit"
-                                    icon="rocket"
-                                    disabled={enviarriesgoDisabled}>
-                                    Enviar a Riesgo
-                                </Button>
+                                <div className="new-requirement-list component">
+                                    <div className="wrap-table">
+                                        <Table
+                                            title={() => "Actas de acuerdos realizados"}
+                                            bordered
+                                            locale={{emptyText: 'No hay datos'}}
+                                            size="small"
+                                            scroll={{x: 800}}
+                                            loading={loadingChange}
+                                            dataSource={dataSourceChange}
+                                            columns={columns_agreement} />
+                                    </div>
+                                </div>
                             </FormItem>
                         </Col>
                         <Col span={12}>
-                            <FormItem
-                                label={'Fecha de Envio'}
-                                {...formItemLayout}
-                            >
+                            <FormItem label={'Consideraciones adicionales:'}>
+                                <TextArea rows={11} />
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            Afectación a la línea base del proyecto
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={8}>
+                            <FormItem label={'Termino:'} {...formItemLayout} >
                                 <DatePicker
-                                    disabled={fenvioDisabled}
-                                    value={moment(evr_FechaEnvio, this.dateformat)}
-                                    format={dateFormat}
-                                />
-                            </FormItem>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span={12}>
-                            <FormItem
-                                label={'Prioridad'}
-                                {...formItemLayout}
-                            >
-                                {getFieldDecorator('pri_Codigo', {
-                                    rules: [{required: true, message: 'Seleccionar Prioridad'}],
-                                    initialValue: pri_Codigo
-                                })(
-                                    <Select
-                                        showSearch
-                                        disabled={prioridadDisabled}
-                                        placeholder="Seleccione Prioridad">
-                                        {OptionPrioridad}
-                                    </Select>
-                                )}
-                            </FormItem>
-                        </Col>
-                        <Col span={12}>
-                            <FormItem
-                                label={'Estado'}
-                                {...formItemLayout}
-                            >
-                                <Select
-                                    showSearch
-                                    disabled={estadoDisabled}
-                                    placeholder="Seleccione Estado"
-                                    value={evr_Estado}>
-                                    {OptionEstado}
-                                </Select>
-                            </FormItem>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span={12}>
-                            <FormItem
-                                label={'Observación'}
-                                {...formItemLayout}
-                            >
-                                {getFieldDecorator('evr_Observacion', {
-                                    rules: [{required: true, message: 'Ingrese una observación'}],
-                                    initialValue: evr_Observacion
-                                })(
-                                    <TextArea
-                                        disabled={observacionDisabled}
-                                        placeholder="Observación"
-                                    />
-                                )}
-                            </FormItem>
-                        </Col>
-                        <Col span={12}>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span={12}>
-                            <FormItem
-                                label={'Fecha de Respuesta'}
-                                {...formItemLayout}
-                            >
-                                <DatePicker
-                                    disabled={frespuestaDisabled}
+                                    disabled={true}
                                     value={moment(evr_FechaRespuesta, this.dateformat)}
-                                    format={dateFormat}
-                                />
+                                    format={dateFormat}/>
                             </FormItem>
                         </Col>
-                        <Col span={12}>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span={12}>
-                            <FormItem
-                                label={'Informe'}
-                                {...formItemLayout}
-                            >
-                                <TextArea
-                                    disabled={informeDisabled}
-                                    placeholder="Resumen de informe emitido por Gestión de Riesgo"
-                                    value={evr_Informe}
-                                />
+                        <Col span={8}>
+                            <FormItem label={'Tiempo adicional(dias):'} {...formItemLayout}>
+                                <Input />
                             </FormItem>
                         </Col>
-                        <Col span={12}>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span={12}>
-                            <FormItem
-                                label={'Impacto'}
-                                {...formItemLayout}
-                            >
-                                <Select
-                                    showSearch
-                                    disabled={impactoDisabled}
-                                    placeholder="Seleccione Impacto"
-                                    value={evr_Impacto}>
-                                    {OptionImpacto}
-                                </Select>
+                        <Col span={8}>
+                            <FormItem label={'Nuevo termino:'} {...formItemLayout} >
+                                <DatePicker
+                                    disabled={true}
+                                    value={moment(evr_FechaRespuesta, this.dateformat)}
+                                    format={dateFormat}/>
                             </FormItem>
                         </Col>
                     </Row>
                     <Row>
-                        <Col span={12}>
-                            <FormItem
-                                label={'Adjunto'}
-                                {...formItemLayout}
-                            >
-                                <Button
-                                    icon="download"
-                                    //onClick={}
-                                    disabled={downloadDisabled}>
-                                    Download
-                                </Button>
+                        <Col span={8}>
+                            <FormItem label={'Costo (S/):'} {...formItemLayout}>
+                                <Input disabled={true} />
+                            </FormItem>
+                        </Col>
+                        <Col span={8}>
+                            <FormItem label={'Costos adicionales (S/):'} {...formItemLayout}>
+                                <Input />
+                            </FormItem>
+                        </Col>
+                        <Col span={8}>
+                            <FormItem label={'Nuevo Costo (S/):'} {...formItemLayout}>
+                                <Input disabled={true}/>
                             </FormItem>
                         </Col>
                     </Row>
-                    {/*Cambios de Requerimientos*/}
-                    <FormItem>
-                        <div className="flow-requirement component">
-                            <div className="wrap-table">
-                                <Table
-                                    title={this.titleTable.bind(this)}
-                                    bordered
-                                    locale={{emptyText: 'No hay datos'}}
-                                    size="small"
-                                    scroll={{x: 800}}
-                                    loading={loadingChange}
-                                    dataSource={dataSourceChange}
-                                    columns={columns_requirement_change} />
-                            </div>
-                        </div>
-                    </FormItem>
+                    <Row>
+                        <Button type="primary">Guardar</Button>
+                    </Row>
                 </div>
             </Form>
         );
