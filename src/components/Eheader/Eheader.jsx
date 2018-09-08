acc6864b9aef81ccf332c4b971685937aec25705
Waldo J <w.saccaco@gmail.com>
2018-09-08 12:57:13 -0500

    update fix

--- src/components/Eheader/Eheader.jsx
+++ src/components/Eheader/Eheader.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import {Layout, Menu} from 'antd'
-
+import {Layout, Menu, Avatar} from 'antd'
+import logo from './../../assert/logo.png'
+import { NavLink } from 'react-router-dom'
 let {Header} = Layout;
 
 export default class Eheader extends Component {
@@ -8,18 +9,28 @@ export default class Eheader extends Component {
 
   render() {
     return (
-        <Header>
-          <div className="logo" />
-          <Menu
+        <Header style={{display: 'flex', justifyContent: 'space-between'}}>
+          <div style={{display: 'flex'}}>
+            <NavLink to="/change/main">
+              <div className="logo">
+                <img src={logo} style={{maxHeight: '64px', display: 'flex', margin: '0 5px'}} alt=""/>
+              </div>
+            </NavLink>
+            <Menu
               theme="dark"
               mode="horizontal"
-              defaultSelectedKeys={['2']}
+              defaultSelectedKeys={['1']}
               style={{ lineHeight: '64px' }}
-          >
-            <Menu.Item key="1">Módulo 1</Menu.Item>
-            <Menu.Item key="2">Módulo 2</Menu.Item>
-            <Menu.Item key="3">Módulo 3</Menu.Item>
-          </Menu>
+            >
+              <Menu.Item key="1">Gestión de Cambio</Menu.Item>
+              <Menu.Item key="3">Módulo 3</Menu.Item>
+            </Menu>
+          </div>
+          <div>
+            <Avatar style={{background: '#87d068'}} size="large">
+              W
+            </Avatar>
+          </div>
         </Header>
     );
   }
