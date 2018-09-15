61c4460152613304c7a069a187d44da2a67863c1
Waldo J <w.saccaco@gmail.com>
2018-09-15 15:15:47 -0500

    update

--- src/page/leaderTechnical/LeaderTechnical.js
+++ src/page/leaderTechnical/LeaderTechnical.js
@@ -3,59 +3,60 @@ import {Col, Row, Card, InputNumber, Table} from 'antd';
 import LeaderTechnicalLayout from '../../layout/leaderTechnical/leaderTechnical';
 
 import './LeaderTechnical.css';
+import http from '../../service/http';
+
+template = [
+  {
+    title: 'Codigo',
+    dataIndex: 'rfc_Codigo',
+    key: 'rfc_Codigo',
+  },
+  {
+    title: 'Proyecto',
+    dataIndex: 'pro_Nombre',
+    key: 'pro_Nombre'
+  }, {
+    title: 'RFC',
+    dataIndex: 'rfc_Asunto',
+    key: 'rfc_Asunto',
+  }, {
+    title: 'Codigo Req',
+    dataIndex: 'lir_Codigo',
+    key: 'lir_Codigo',
+  }, {
+    title: 'Requerimiento',
+    dataIndex: 'lir_Nombre',
+    key: 'lir_Nombre',
+  }, {
+    title: 'Estado',
+    dataIndex: 'est_Estado',
+    key: 'est_Estado',
+  }, {
+    title: 'Acciones',
+    width: 100,
+    fixed: 'right',
+    render: text => {
+      return <a href="#" onClick={this._openModal.bind(this)}>Evaluar</a>;
+    },
+  }];
 
 export default class LeaderTechnical extends Component {
 
   constructor(props) {
     super(props);
+    let {params: {id}} = props.match;
     this.state = {
+      dataSource: [],
+      dataSourcePendiente: [],
       items: [],
       text: '',
       FormTechnicalEvalue: '',
       visibleModal: true,
+      id
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  columnsRiskPending = [
-    {
-      title: 'Código',
-      dataIndex: 'rfc_Codigo',
-      key: 'rfc_Codigo',
-      render: text => `P00${text}`,
-    }, {
-      title: 'Fecha Recepción',
-      dataIndex: 'evr_FechaEnvio',
-      key: 'evr_FechaEnvio',
-    }, {
-      title: 'Fecha Límite',
-      dataIndex: 'evr_FechaLimite',
-      key: 'evr_FechaLimite',
-    }, {
-      title: 'Asunto',
-      dataIndex: 'rfc_Asunto',
-      key: 'rfc_Asunto',
-    }, {
-      title: 'Prioridad',
-      dataIndex: 'pri_Descripcion',
-      key: 'pri_Descripcion',
-    }, {
-      title: 'Estado',
-      dataIndex: 'esr_Descripcion',
-      key: 'esr_Descripcion',
-    }, {
-      title: 'Acciones',
-      render: text => {
-        return <a href="#" onClick={this._openModal.bind(this)}>Evaluar</a>;
-      },
-    }];
-
-  fakeData = [
-    {
-      pri_Descripcion: 'Asunto',
-    },
-  ];
 
   _openModal() {
     import('../../components/formEvalueTechnical/formEvalueTechnical.js').then(
@@ -73,37 +74,62 @@ export default class LeaderTechnical extends Component {
     })
   }
 
+  fetchRequerimientoTecnico() {
+    let {id} = this.state;
+    http(`RequerimientoTecnico/${id}`, 'GET', {}, (response) => {
+      this.setState({
+        dataSource: response
+      });
+    });
+  }
+
+  fetchRequerimientoTecnicoPendiente() {
+    let {id} = this.state;
+    http(`RequerimientoTecnicoPendiente/${id}`, 'GET', {}, (response) => {
+      this.setState({
+        dataSourcePendiente: response
+      });
+    });
+  }
+
   componentDidMount(){
     this._openModal();
+    this.fetchRequerimientoTecnico();
+    this.fetchRequerimientoTecnicoPendiente();
   }
 
   render() {
-    let {loading, FormTechnicalEvalue, visibleModal} = this.state;
+    let {loading, FormTechnicalEvalue, visibleModal, dataSource, dataSourcePendiente} = this.state;
     return (
       <LeaderTechnicalLayout>
         <Row justify="center" type="flex">
           <Col>
-            <Table
-              title={() => <strong>Lista de Evaluaciones tecnicas
-                Pendientes</strong>}
-              columns={this.columnsRiskPending}
-              dataSource={this.fakeData}
-              size="middle"
-              loading={loading}
-              bordered
-              scroll={{x: 1300}}/>
+            <div style={{width: '100%', maxWidth: '1200px'}}>
+              <Table
+                title={() => <strong>Lista de Evaluaciones tecnicas
+                  Pendientes</strong>}
+                columns={this.columnsRiskPending}
+                dataSource={dataSource}
+                size="middle"
+                loading={loading}
+                bordered
+                scroll={{x: 1500}}/>
+            </div>
+
           </Col>
 
           <Col>
-            <Table
-              style={{marginTop: '25px'}}
-              title={() => <strong>Historial de evaluaciones</strong>}
-              columns={this.columnsRiskPending}
-              dataSource={[]}
-              size="middle"
-              loading={loading}
-              bordered
-              scroll={{x: 1300}}/>
+            <div style={{width: '100%', maxWidth: '1200px'}}>
+              <Table
+                style={{marginTop: '25px'}}
+                title={() => <strong>Historial de evaluaciones</strong>}
+                columns={this.columnsRiskPending}
+                dataSource={dataSourcePendiente}
+                size="middle"
+                loading={loading}
+                bordered
+                scroll={{x: 1300}}/>
+            </div>
           </Col>
         </Row>
         {FormTechnicalEvalue && visibleModal
